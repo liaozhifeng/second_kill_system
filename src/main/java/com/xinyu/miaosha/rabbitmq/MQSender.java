6e96820e23bac81915722e75301a93d9a4e9599a
liaozhifeng <1528395405@qq.com>
2021-12-18 00:07:10 +0800

    添加了rabbitMq的四种基本方式

--- src/main/java/com/xinyu/miaosha/rabbitmq/MQSender.java
+++ src/main/java/com/xinyu/miaosha/rabbitmq/MQSender.java
@@ -4,7 +4,11 @@ import com.xinyu.miaosha.service.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.HeadersExchange;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,5 +23,31 @@ public class MQSender {
         String msg = RedisService.beanToString(message);
         log.info("send message:" + msg);
         amqpTemplate.convertAndSend(MQConfig.QUEUE, msg);
+    }
+
+    public void sendTopic(Object message) {
+        String msg = RedisService.beanToString(message);
+        log.info("send topic message:" + msg);
+        amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, MQConfig.ROUTING_KEY1,
+                msg + "1");
+        amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, "topic.key2",
+                msg + "2");
+    }
+
+    public void sendFanout(Object message) {
+        String msg = RedisService.beanToString(message);
+        log.info("send fanout message:" + msg);
+        amqpTemplate.convertAndSend(MQConfig.FANOUT_EXCHANGE, "",
+                msg + "1");
+    }
+
+    public void sendHeaders(Object message) {
+        String msg = RedisService.beanToString(message);
+        log.info("send header message:" + msg);
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("header1", "value1");
+        properties.setHeader("header2", "value2");
+        Message headers = new Message(msg.getBytes(), properties);
+        amqpTemplate.convertAndSend(MQConfig.HEADERS_EXCHANGE, "", headers);
     }
 }
